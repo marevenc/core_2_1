a2bc25e2eab0781d738569f5701bdcaaee662e37
Mariya Kosheleva <marevenc@gmail.com>
2020-07-11 22:34:13 +0300

    Исправила форматирование

--- src/Calculator.java
+++ src/Calculator.java
@@ -7,11 +7,13 @@ public class Calculator<T extends Number> {
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
     BinaryOperator<Double> divide = (x, y) -> {
-                                            if(y != 0)
-                                                return x/y;
-                                            else {System.out.println("Деление на ноль!");
-                                            return Double.MAX_VALUE;}
-                                            };
+                                                if(y != 0)
+                                                    return x/y;
+                                                else {
+                                                    System.out.println("Деление на ноль!");
+                                                    return Double.MAX_VALUE;
+                                                }
+                                              };
 
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
